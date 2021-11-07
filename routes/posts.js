20bb0363e96bab5cc2b9352b76e25180657c10df
arabelhousseyn <potency.football@gmail.com>
2021-11-07 13:38:37 +0000

    replace all posts with main route "/"

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const postController = require('../controller/postController')
-router.get('/',(req,res) => {
-    console.log('we are in the main of the posts')
-})
-
+router.get('/',postController.getPosts)
 router.post('/',postController.insertPost)
-router.get('/all-posts',postController.getPosts)
 router.put('/update-post/:id',postController.updatePost)
 router.delete('/delete-post/:id',postController.deletePost)
 
